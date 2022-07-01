05069bc497e53520aa3bd72bf86d910abdf014b2
Jock1323 <mirsidiq2002@gmail.com>
2022-07-01 16:00:00 +0500

    add some keys

--- js/app.js
+++ js/app.js
@@ -9,10 +9,11 @@ let elContainer=document.querySelector('.container__inner');
 let input;
 
 let data = (country) => {
-    fetch(`https:api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=274f61e74d7f8090a7f9dd2f7a0c3ec2`)
+    fetch(`https:api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=277e160f5af509c9f6e384d7cbe3501c`)
         .then(res => res.json())
         .then(data => {
             renderWeather(data);
+            // 274f61e74d7f8090a7f9dd2f7a0c3ec2
         })
 }
 elForm.addEventListener('submit', e => {
